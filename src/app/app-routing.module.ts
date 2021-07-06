59244fe0152f8c3c0ed916473aeb51efa1610a42
neritoo <ezegavilan95@gmail.com>
2021-07-06 13:23:41 -0300

    Home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
